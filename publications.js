7c8dcdc45384f568dc15d55d264dd7f219dff42a
jsancheza04 <j.sancheza@uniandes.edu.co>
2024-01-05 17:32:15 -0500

    Updates 1/5

--- publications.js
+++ publications.js
@@ -4,8 +4,8 @@ const publications = [
         "type": "publication",
         "title": "The Mental Health of Caregivers and Young Children in Conflict-Affected Settings",
         "link": "https://doi.org/10.1257/pandp.20231017",
-        "authors": "with <i>Jorge Cuartas</i> and <i>Andrés Moya</i>, 2023.",
-        "journal": "<b>AEA Papers and Proceedings</b>, Volume 113, Pages 336-41",
+        "authors": "<i>Sánchez-Ariza, J.</i>, Cuartas, J. & Moya, A (2023)",
+        "journal": "<b>AEA Papers and Proceedings</b>, 113, 336-41",
         "short_abstract": "We analyze the effects of conflict-related violence on the mental health of caregivers and young children, as well as the role of caregivers' mental health in explaining the toll of conflict on early childhood mental health",
         "abstract": "We analyze the effects of conflict-related violence on the mental health of caregivers and young children, as well as the role of caregivers' mental health in explaining the toll of conflict on early childhood mental health. Although an upcoming body of work has demonstrated the link between caregivers' and children's mental health, the role of caregivers' psychological constraints has been largely ignored in research and programming in conflict-affected settings. We demonstrate how these constraints play a role in the intergenerational transmission of mental health problems and emphasize the urgency of addressing caregivers' mental health within early childhood development programs in conflict-affected settings."
     },
@@ -13,8 +13,8 @@ const publications = [
         "id": 2,
         "title": "The relationship between multidimensional poverty, income poverty and youth depressive symptoms: cross-sectional evidence from Mexico, South Africa and Colombia",
         "type": "publication",
-        "link": "https://doi.org/10.1136/bmjgh-2022-006960",
-        "authors": "Zimmerman A, Lund C, Araya R, Hessel P, <i>Sánchez J</i>, Garman E, Evans-Lacko S, Díaz Y, Avendano-Pabon M, 2022.",
+        "link": "https://gh.bmj.com/content/7/1/e006960",
+        "authors": "Zimmerman A, Lund C, Araya R, Hessel P, <i>Sánchez J</i>, Garman E, Evans-Lacko S, Díaz Y, Avendano-Pabon M. (2022)",
         "journal": "<b>BMJ Global Health</b> 2022;7:e006960.",
         "short_abstract": "The relationship between multidimensional poverty, income poverty and youth depressive symptoms: cross-sectional evidence from Mexico, South Africa, and Colombia.",
         "abstract": "This paper explores the relationship between multidimensional poverty, income poverty, and youth depressive symptoms using cross-sectional evidence from Mexico, South Africa, and Colombia. It investigates how various dimensions of poverty in these countries are associated with depressive symptoms among young people. The findings suggest that addressing multidimensional poverty is crucial for improving youth mental health in low- and middle-income countries."
@@ -24,7 +24,7 @@ const publications = [
         "title": "Family play, reading, and other stimulation and early childhood development in five low-and-middle-income countries",
         "type": "publication",
         "link": "https://doi.org/10.1111/desc.13404",
-        "authors": "Cuartas, J., McCoy, D., Sánchez, J., Behrman, J., Cappa, C., Donati, G., Heymann, J., Lu, C., Raikes, A., Rao, N., Richter, L., Stein, A., and Yoshikawa, H., 2023.",
+        "authors": "Cuartas, J., McCoy, D., <i>Sánchez, J.</i>, Behrman, J., Cappa, C., Donati, G., Heymann, J., Lu, C., Raikes, A., Rao, N., Richter, L., Stein, A., & Yoshikawa, H. (2023)",
         "journal": "<b>Developmental Science</b>, 00, e13404",
         "short_abstract": "Family play, reading, and other stimulation and early childhood development in five low-and-middle-income countries.",
         "abstract": "This research investigates the impact of family play, reading, and other forms of stimulation on early childhood development in five low- and middle-income countries. It emphasizes the significance of these activities in promoting early childhood development and highlights their role in enhancing cognitive and socio-emotional outcomes in children."
@@ -32,11 +32,12 @@ const publications = [
     {
         "id": 4,
         "type": "publication",
-        "authors": "<i>Sánchez-Ariza J</i>, Moya A, Cuartas J.",
+        "authors": "<i>Sánchez-Ariza J</i>, Moya A, Cuartas J. (2022)",
+        "link": "https://papers.ssrn.com/sol3/papers.cfm?abstract_id=4230362",
         "journal": "<b>Documento CEDE</b>, #34",
         "title": "Conflict, Parenting, and Early Childhood Mental Health in Conflict-affected Settings: Evidence from Colombia.",
-        "abstract_short": "This study provides evidence on the relationship between conflict, parenting practices, and early childhood mental health in conflict-affected settings, with a focus on Colombia. It explores how exposure to conflict influences parenting practices and the subsequent impact on early childhood mental health outcomes. The findings have important implications.",
-        "abstract": "This study provides evidence on the relationship between conflict, parenting practices, and early childhood mental health in conflict-affected settings, with a focus on Colombia. It explores how exposure to conflict influences parenting practices and the subsequent impact on early childhood mental health outcomes. The findings have important implications for interventions in conflict-affected regions."
+        "short_abstract": "We examine the effect of the exposure (or reduction in the exposure) to conflictrelated violence on parental mental health, caregivers' parenting stress and responsive caregiving, and early childhood mental health. We use data collected from the impact evaluation of a psychosocial group intervention in Tumaco, Colombia, a community chronically affected by the armed conflict. ",
+        "abstract": "We examine the effect of the exposure (or reduction in the exposure) to conflictrelated violence on parental mental health, caregivers' parenting stress and responsive caregiving, and early childhood mental health. We use data collected from the impact evaluation of a psychosocial group intervention in Tumaco, Colombia, a community chronically affected by the armed conflict. Using an Instrumental Variables approach, we use the program's randomization into cohorts and staggered design of the data collection to exploit a natural experiment in which the armed groups in the municipality agreed to a Truce and municipal violence rates dropped between data collections. We find that the exposure to recent conflict-related violence had negative effects across the four main dimensions: increased parental mental health problems in 0.68 sd (SE=0.342 ; q-value=0.074), increased parenting stress in 0.76 sd (SE=0.389 ; q-value=0.074), reduced responsive caregiving in -1.023 sd (SE=0.434 ; q-value=0.074) and increased child mental health problems in 0.556 sd (SE=0.343 ; q-value=0.074). By providing causal evidence on the direct effect of conflict-related violence on parenting outcomes, we conduct an exploratory mediation analysis to assess whether parental mental health, parenting stress and responsive caregiving behaviors may partially account for the association between violence and child mental health. Yet, correlational links between violence and parental mental health and parenting behavior outcomes as mediators suggest important associations for understanding children's mental health vulnerability in conflict settings. "
     },
     {
         "id": 5,
